7f8e5b18352f81b3b312b7c1ec9fc614f13dd204
hafizameerhamza <hafizameerhamza1@gmail.com>
2024-07-31 23:56:22 +0500

    Archive posts list with pagination and no content template

--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -31,10 +31,10 @@
         <?php 
             the_content(); 
 
-            wp_link_pages( array(
-                'before' => '<div class="page-links">' . esc_html__( 'pages:', 'ninestars'),
-                'after'  => '</div>'
-            ) );
+            // wp_link_pages( array(
+            //     'before' => '<div class="page-links">' . esc_html__( 'pages:', 'ninestars'),
+            //     'after'  => '</div>'
+            // ) );
         ?>
     </div>
     <?php endif; ?>
